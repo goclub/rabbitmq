62ead0bdfc576b26049fc5931dbcb9e723187972
nimoc <nimocc@126.com>
2022-02-03 21:54:42 +0800

    Update proxy.go

--- proxy.go
+++ proxy.go
@@ -156,10 +156,10 @@ func (channel *ProxyChannel) Consume(consume Consume) (<-chan amqp.Delivery,erro
 }
 
 func (channel *ProxyChannel) Publish(publish Publish) (err error) {
-	exchange, key, mandatory,immediate, msg := publish.Flat()
 	if publish.Msg.MessageId == "" {
 		publish.Msg.MessageId = MessageID()
 	}
+	exchange, key, mandatory,immediate, msg := publish.Flat()
 	return channel.Channel.Publish(exchange, key, mandatory,immediate, msg)
 }
 
